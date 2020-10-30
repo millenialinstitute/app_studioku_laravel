29520402702f5e880a0bf82b6b7dbb401d3ca428
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-10-30 20:22:30 +0700

    display profile data in dashboard member

--- app/Http/Controllers/Member/ProfileController.php
+++ app/Http/Controllers/Member/ProfileController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function index ( ) 
     {
-    	return view('member.profile');
+
+    	return view('member.profile' , [
+    						'user' => Auth::user(),
+			    		]);
     }
 }
--- resources/views/member/profile.blade.php
+++ resources/views/member/profile.blade.php
@@ -3,7 +3,66 @@
 @section('profile' , 'active')
 @section('body')
 
-<h1>Profile</h1>
+<div class="row">
+	<div class="col">
+		<div class="card card-profile">
+			<div class="profile">
+				<img src="{{ asset('storage/users/' . $user->image) }}" alt="{{ $user->name }}" style="width: 100%">
+			</div>
+			<div class="text-center">
+				<h2>{{ $user->name }}</h2>
+				<h3>{{ $user->email }}</h3>
+			</div>
+			<p>Cilacap , Jawa Tengah , Indonesia</p>
+			<p>Member sejak {{ substr($user->created_at, 0 , 10) }}</p>
+		</div>
+	</div>
+	<div class="col-2">
+		<div class="card row list-data">
+			<div class="col list-item text-center" style="border-right: 2px solid #a8afcc;">
+				<h1 class="value">20</h1>
+				<h4 class="key">Unduhan</h4>
+			</div>
+			<div class="col list-item text-center">
+				<h1 class="value">20</h1>
+				<h4 class="key">Favorite</h4>
+			</div>
+			<div class="col list-item text-center" style="border-left: 2px solid #a8afcc;">
+				<h1 class="value">20</h1>
+				<h4 class="key">Koleksi</h4>
+			</div>
+		</div>
+		<div class="card biodata">
+			<div class="row">
+				<div class="col">Nama Lengkap</div>
+				<div class="col-2">{{ $user->name }}</div>
+			</div>
+			<div class="row">
+				<div class="col">Email</div>
+				<div class="col-2">{{ $user->email }}</div>
+			</div>
+			<div class="row">
+				<div class="col">No.Hp</div>
+				<div class="col-2">0892919291929</div>
+			</div>
+			<div class="row">
+				<div class="col">Alamat</div>
+				<div class="col-2">
+					{{ $user->member->address }}
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">Bergabung sejak</div>
+				<div class="col-2">{{ substr($user->created_at, 0 , 10) }}</div>
+			</div>
+		</div>
+	</div>
+</div>			
+
+<div class="card buy">
+	<img src="{{ asset('assets/dashboard/icons/cart_icon_orange.svg') }}" class="mr-1" alt="cart icon">
+	Pembelian (20 Item)
+</div>
 
 @endsection
 	
