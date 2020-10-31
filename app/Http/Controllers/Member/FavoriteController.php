7b1c8b5304a835664622072479684a79af4e62d8
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-10-31 19:18:30 +0700

    display item liked in dashboard member

--- app/Http/Controllers/Member/FavoriteController.php
+++ app/Http/Controllers/Member/FavoriteController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Member;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\ItemLike;
 
 class FavoriteController extends Controller
 {
     public function index ( ) 
     {
-    	return view('member.favorite');
+    	$items = ItemLike::where('user_id' , Auth::id())->latest()->get();
+
+    	return view('member.favorite' , [
+    								'user' => Auth::user(),
+    								'items' => $items,
+						    	]);
     }
 }
--- resources/views/member/favorite.blade.php
+++ resources/views/member/favorite.blade.php
@@ -3,7 +3,23 @@
 @section('favorite' , 'active')
 @section('body')
 
-<h1>Favorite</h1>
+<h3 class="title-section">Item Disukai</h3>
+
+@forelse($items->chunk(4) as $chunk)
+	<div class="row">
+		@foreach($chunk as $item)
+			@php  $item = $item->item  @endphp
+			<div class="col">
+				<a href="{{ url('/item/detail/' . $item->id) }}" class="item-thumbnail">
+					<img src="{{ asset('storage/photos/' . $item->image) }}" alt="image" class="img">
+				</a>
+			</div>
+		@endforeach
+	</div>
+@empty
+	<h3>Tidak ada item</h3>
+@endforelse
+	
 
 @endsection
 	
