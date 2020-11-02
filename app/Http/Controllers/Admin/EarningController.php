84f61fbc754fd4c07d35db98483d63c9e6f9c08b
BJ-stack-art <58924072+BJ-stack-art@users.noreply.github.com>
2020-11-02 13:08:08 +0700

    display saldo in dashboard adminc

--- app/Http/Controllers/Admin/EarningController.php
+++ app/Http/Controllers/Admin/EarningController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\SaldoStatistic;
 
 class EarningController extends Controller
 {
    public function index ( ) 
    {
-   		return view('admin.earning');
+   		$dataAll = SaldoStatistic::get();
+   		$totalSaldo = 0;
+   		foreach ($dataAll as $data) {
+   			$totalSaldo+= $data->total;
+   		}
+
+   		if(strlen($totalSaldo) > 6) {
+   			$totalSaldo = (int)substr($totalSaldo, 0, -5) / 10;
+   			$totalScope = 'Jt';
+   		}
+
+   		return view('admin.earning' , [
+								'user'       => Auth::user(),
+								'total'      => $totalSaldo, 
+								'totalScope' => $totalScope,
+				   		]);
    }			
 }
--- resources/views/admin/earning.blade.php
+++ resources/views/admin/earning.blade.php
@@ -6,7 +6,20 @@
 
 <div class="row">
 	<div class="col">
-		<div class="card-earning">
+		<div class="card-earning mb-3">
+			<img src="{{ asset('/assets/dashboard/illustration/earning_illustration.svg') }}" alt="earning">
+			<h2>{{ $total }} {{ $totalScope }}</h2>
+			<p>Total Pendapatan (Total yang didapat dari penjualan item)</p>
+		</div>
+	</div>
+	<div class="col-2">
+		
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<div class="card-earning mb-3">
 			<img src="{{ asset('/assets/dashboard/illustration/earning_illustration.svg') }}" alt="earning">
 			<h2>30 Jt</h2>
 			<p>Total Pendapatan (Total yang didapat dari penjualan item)</p>
@@ -19,20 +32,7 @@
 
 <div class="row">
 	<div class="col">
-		<div class="card-earning">
-			<img src="{{ asset('/assets/dashboard/illustration/earning_illustration.svg') }}" alt="earning">
-			<h2>30 Jt</h2>
-			<p>Total Pendapatan (Total yang didapat dari penjualan item)</p>
-		</div>
-	</div>
-	<div class="col-2">
-		
-	</div>
-</div>
-
-<div class="row">
-	<div class="col">
-		<div class="card-earning">
+		<div class="card-earning mb-3">
 			<img src="{{ asset('/assets/dashboard/illustration/earning_illustration.svg') }}" alt="earning">
 			<h2>30 Jt</h2>
 			<p>Total Pendapatan (Total yang didapat dari penjualan item)</p>
